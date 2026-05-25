56763fa4e8e5651f01f6db932557790125ae8121
Will Skou <WillSkou@gmail.com>
2026-05-25 11:01:25 -0500

    Added get color data api call and created list of ColorData objects in main dart

---
+++ CarColorFrequenceApp/car_color_frequence_app/android/app/src/main/kotlin/com/example/car_color_frequence_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.car_color_frequence_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ CarColorFrequenceApp/car_color_frequence_app/lib/apiComm.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // For JSON decoding
+import './colorData.dart';
+
+class Apicomm
+{
+static Future<List<ColorData>> fetchData() async {
+  final url = Uri.parse('http://192.168.50.89:779/CarColorFrequency');
+  
+  try {
+    final response = await http.get(url);
+    
+    if (response.statusCode == 200) {
+      final List<dynamic> parsedJson = jsonDecode(response.body);
+
+      List<ColorData> colorData = parsedJson.map((json) => ColorData.fromJson(json)).toList();    
+      return colorData; 
+    } else {
+      print('Request failed with status: ${response.statusCode}');
+    }
+  } catch (e) {
+    print('Error fetching data: $e');
+  }
+  return [];
+}
+}
