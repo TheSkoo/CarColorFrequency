5cac46f9dfe04a3407dd2aa075094d0efdf1dd7d
Will Skou <WillSkou@gmail.com>
2026-05-28 09:12:53 -0500

    dynamically adding buttons with fore and back ground colors

--- CarColorFrequenceApp/car_color_frequence_app/lib/main.dart
+++ CarColorFrequenceApp/car_color_frequence_app/lib/main.dart
@@ -56,7 +56,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
- var colorData;
+ List<ColorData> colorData = [];
 
   @override
   void initState() {
@@ -65,20 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
         setState(() {
           colorData = value;
         });
-  });    int a = 0; // This has to be here for the Future<T> fetchData() to return a value
-  }
-
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  });
   }
 
   @override
@@ -89,48 +76,19 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Wrap(
+    spacing: 8.0, // Space between buttons
+    runSpacing: 8.0, // gap between lines
+    children: colorData.map((label) {
+      return ElevatedButton(
+        onPressed: () => print('Pressed $label'),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color(label.backgroundColorRGB + 4278190080), // convert to ARGB by adding alpha value
+          foregroundColor: Color(label.foregroundColorRGB + 4278190080),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        child: Text(label.color),
+      );
+    }).toList(),
     );
   }
 }
